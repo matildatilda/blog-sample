343a1a6fe76269d31ef4ce9ed2980cad62099f13
matildatilda <melty.bombom@gmail.com>
2016-01-25 00:48:08 +0000

    1.0

---
+++ php/DbManager.php
@@ -0,0 +1,28 @@
+<?php
+class DbManager
+{
+    protected $dbh = null;
+    
+    public function ConnectDb()
+    {
+        $dsn = "mysql:dbname=blogsample;host=127.0.0.1";
+        $user = getenv('C9_USER');
+        $password = "";
+        
+        try
+        {
+            $this->dbh = new PDO($dsn, $user, $password);
+            $this->dbh->query('SET NAMES utf8');
+
+        }
+        catch (PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+        }                
+    }
+    
+    public function DisConnectDb()
+    {
+        $this->dbh = null;   
+    }
+}
